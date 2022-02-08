3470ef5ef69d1e6cddaba6b1d02994d7822321d6
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-08 11:31:34 +0000

    thought schema completed

---
+++ src/models/Thought.js
@@ -0,0 +1,26 @@
+const { Schema } = require("mongoose");
+const moment = require("moment");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 280,
+  },
+
+  createdAt: {
+    type: Date,
+    default: moment(),
+  },
+
+  username: {
+    type: String,
+    required: true,
+  },
+
+  reactions: [reactionSchema],
+});
+
+module.exports = thoughtSchema;
