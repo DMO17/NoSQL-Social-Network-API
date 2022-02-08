0604d013fc22b98e5837b154baf68481d595b90f
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-08 14:01:03 +0000

    created user , thoughs and response data

--- src/seeds/data/index.js
+++ src/seeds/data/index.js
@@ -1,4 +1,4 @@
-const users = [
+const userssss = [
   {
     username: "bobsmith",
     email: "bobsmith@email.com",
@@ -32,4 +32,83 @@ const users = [
   },
 ];
 
-module.exports = users;
+const users = [
+  {
+    username: "brailsstain",
+    email: "kell253@yaachea.com",
+    thoughts: [],
+  },
+  {
+    username: "impossiblerainy",
+    email: "waverunner@nroeor.com",
+    thoughts: [],
+  },
+  {
+    username: "gownshampoo",
+    email: "konstantinsolovev96@midtoys.com",
+    thoughts: [],
+  },
+  {
+    username: "perturbednomination",
+    email: "kakise@hansgu.com",
+    thoughts: [],
+  },
+  {
+    username: "prisonerpretzel",
+    email: "britofilho@gmailer.com",
+    thoughts: [],
+  },
+  {
+    username: "rolethrifty",
+    email: "komissarov1997@omtecha.com",
+    thoughts: [],
+  },
+  {
+    username: "sweatdingy",
+    email: "deplint@nalsci.com",
+    thoughts: [],
+  },
+  {
+    username: "watchfulcollect",
+    email: "jimfrehytht@akufry.com",
+    thoughts: [],
+  },
+  {
+    username: "southeastsubdued",
+    email: "kevozic@anticheatpd.com",
+    thoughts: [],
+  },
+];
+
+const thoughts = [
+  "Earth is a flat disk",
+  "The wedding industrial complex is predatory capitalism",
+  "The Office US is far better and funnier than the UK original",
+  "Star Wars should have stopped at one film.",
+  "People drink gin because its trendy",
+  "I cant stand cheese.",
+  "I hate avocados, but I love guacamole.",
+  "The Beatles are overrated",
+  "I have no desire to ever visit theme parks",
+  "I dont like Breaking Bad, The Walking Dead or Peaky Blinders",
+  "Chewing gum is disgusting. Football is boring. Chaplin is funnier than Keaton. Plain choc digestives are better than milk choc ones.",
+  "Karaoke is only fun if you are a good singer and love attention. It sucks for the rest of us.",
+];
+
+const responses = [
+  "A watched pot should not throw stones.",
+  "Yes, of course.",
+  "why not loool",
+  "Word!, 100",
+  "Do you own a fax machine? If not, dont do it.",
+  "Doing something is better than doing nothing. Unless its going to get you in jail or in parliament. Or one after the other.",
+  "Do you own a goat? If not, dont do it.",
+  "Becoming a poacher is never a good idea ethically. Sorry, did I mis-read the statement?",
+  "hat which doesnt kill you makes you stronger, right? Unless it almost kills you, and renders you weaker. Being strong is pretty rad though, so go ahead.",
+  "No, not a good idea.",
+  "Yes. Most definitely, yes.",
+  "If youre certain its safe, go ahead.",
+  "Sure, go ahead. Whatever.",
+];
+
+module.exports = { thoughts, responses, users };
