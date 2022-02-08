75f344fe2ad09450e52dc1c9df450f59d96631c9
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-08 11:25:59 +0000

    reaction shema done

---
+++ src/models/Reaction.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    auto: true,
+  },
+
+  reactionBody: {
+    type: String,
+    required: true,
+    max: 280,
+  },
+
+  username: {
+    type: String,
+    ref: "user",
+  },
+
+  createdAt: {
+    type: Date,
+    default: moment(),
+  },
+});
+
+module.exports = reactionSchema;
