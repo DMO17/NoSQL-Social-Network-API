f2f4a2b338d256a3c6e7e2aa0126819f0d220b65
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-08 11:43:56 +0000

    User schema and model created

---
+++ src/models/User.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/test(
+          v
+        );
+      },
+      message: (props) =>
+        `${props.value} | This is an incorrect format for Email address`,
+    },
+  },
+
+  thoughts: [thoughtSchema] , 
+});
+
+
+const User = model('user' , userSchema)
+
+model.exports = User
