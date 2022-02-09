8511aee9b03bae0af614d74f040395076aba4e7d
Dahir Mohamed <dmoabdi98@gmail.com>
2022-02-09 15:15:15 +0000

    user , get , ppost and delete done

--- src/controller/api/users.js
+++ src/controller/api/users.js
@@ -1,8 +1,84 @@
-const getAllUsers = () => {};
-const getUserById = () => {};
-const createUser = () => {};
+const { Users } = require("../../models");
+
+const getAllUsers = async () => {
+  try {
+    const allUsers = await Users.find({});
+
+    return res.json({ success: true, data: allUsers });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get users | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to get users" });
+  }
+};
+
+const getUserById = async () => {
+  try {
+    const { userId } = req.params;
+    const singleUser = await Users.findById(userId);
+
+    if (!singleUser) {
+      console.log("[ERROR]: No user with that ID exists");
+      return res
+        .status(400)
+        .json({ success: false, message: "Failed to get user" });
+    }
+
+    return res.json({ success: true, data: singleUser });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get user | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to get user" });
+  }
+};
+
+const createUser = async () => {
+  try {
+    const { username, email, thoughts = [], friends = [] } = req.body;
+
+    const createUser = await Users.create({
+      username,
+      email,
+      thoughts,
+      friends,
+    });
+
+    return res.json({ success: true, data: createUser });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to to create User | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create User" });
+  }
+};
 const updateUserById = () => {};
-const deleteUserById = () => {};
+
+const deleteUserById = async () => {
+  try {
+    const { userId } = req.params;
+    const singleUser = await Users.findByIdAndDelete(userId);
+
+    if (!singleUser) {
+      console.log("[ERROR]: No user with that ID exists");
+      return res
+        .status(400)
+        .json({ success: false, message: "Failed to delete user" });
+    }
+
+    return res.json({
+      success: true,
+      message: " User Deleted",
+      data: singleUser,
+    });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to get user | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to get user" });
+  }
+};
 
 module.exports = {
   getAllUsers,
